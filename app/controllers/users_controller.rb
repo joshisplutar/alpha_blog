c19bf9dc2738db9863d4bea989e4dcbca3b4ae84
joshisplutar <joshisplutar@gmail.com>
2020-06-14 17:58:34 -0500

    Update pagination for articles and layout changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @articles = @user.articles
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
+  end
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: 5)
   end
   def new
     @user = User.new
@@ -16,7 +20,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "You're edit was a great big success"
-      redirect_to articles_path
+      redirect_to @user
     else
       render 'edit'
     end

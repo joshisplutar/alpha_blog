1492433254e49ceab9e5d6784d1d8d838b96deb0
joshisplutar <joshisplutar@gmail.com>
2020-06-15 01:05:54 -0500

    Update to session controller and logged in functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,20 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
   def show
-    @user = User.find(params[:id])
-    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
+    @articles = @user.articles.paginate(page: params[:page], per_page: 3)
   end
 
   def index
-    @users = User.paginate(page: params[:page], per_page: 5)
+    @users = User.paginate(page: params[:page], per_page: 3 )
   end
   def new
     @user = User.new
   end
 
   def edit
-    @user = User.find(params[:id])
-
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "You're edit was a great big success"
       redirect_to @user
@@ -29,6 +26,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
       if @user.save
+        session[:user_id] = @user.id
         flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, signup was a success"
         redirect_to articles_path
       else
@@ -39,5 +37,9 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end

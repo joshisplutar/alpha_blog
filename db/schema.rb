14e309eb5d78f35397e2f3d9057b4f4a25133d33
joshisplutar <joshisplutar@gmail.com>
2020-06-20 09:11:27 -0500

    Add further testing for admin roles for categories and update to nav

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2020_06_17_013635) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|

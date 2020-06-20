bc6b153d52b6d25c0e4169c2e592930220721ef0
joshisplutar <joshisplutar@gmail.com>
2020-06-20 14:11:46 -0500

    Revert "test search"
    This reverts commit 6f206bae6ebec526257c5222a769374cb8a50316.

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,4 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   resources :categories, except: [:destroy]
-  get 'search', to: 'pages#search'
 end
